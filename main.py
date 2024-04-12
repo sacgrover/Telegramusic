b326796111908d4195060669e5441c0dc89f84c9
Sachin Grover <sacgrover@gmail.com>
2024-04-12 17:17:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -408,11 +408,26 @@ async def help_start(event: types.Message):
     bot_info = await bot.get_me()
     bot_name = bot_info.first_name.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
     bot_username = bot_info.username.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
-    msg = "Hey, I'm *{}*\n".format(bot_name)
-    msg += "_You can use me in inline mode :_\n"
-    msg += "@{} \\(album\\|track\\|artist\\) \\<search\\>\n".format(bot_username)
-    msg += "Or just send an *Deezer* album or track *link* \\!"
+    msg = "Hi, welcome to *{} Bot*\n".format(bot_name)
+    msg += "_To begin searching for music tag me and search via artist, track or album. See examples below :_\n\n"
+    msg += "By Artist\n"
+    msg += "@{} artist Britney Spears\n\n".format(bot_username)
+    msg += "By Track\n"
+    msg += "@{} track Back in Black\n\n".format(bot_username)
+    msg += "By Album\n"
+    msg += "@{} album Abbey Road\n\n".format(bot_username)
+    msg += "For support visit @pepedjen"
     await event.answer(msg, parse_mode="MarkdownV2")
+    
+#async def help_start(event: types.Message):
+#    bot_info = await bot.get_me()
+ #   bot_name = bot_info.first_name.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
+ #   bot_username = bot_info.username.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
+ #   msg = "Hey, I'm *{}*\n".format(bot_name)
+ #   msg += "_You can use me in inline mode :_\n"
+ #   msg += "@{} \\(album\\|track\\|artist\\) \\<search\\>\n".format(bot_username)
+ #   msg += "Or just send an *Deezer* album or track *link* \\!"
+ #   await event.answer(msg, parse_mode="MarkdownV2")
 
 
 @dp.inline_query()
